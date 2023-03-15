4071be4ec0f7921c30e310f37cbffb198507b0e0
Michael Große <michael.grosse@wikimedia.de>
2023-03-15 11:10:09 +0100

    Extract Socket Heating Controller

---
+++ src/SocketHeatingController.php
@@ -0,0 +1,18 @@
+<?php
+
+class SocketHeatingController {
+	public function sendMessage( $message ): void {
+		try {
+			if ( !( $s = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
+				die( 'could not create socket' );
+			}
+			if ( !socket_connect( $s, 'heater.home', 9999 ) ) {
+				die( 'could not connect!' );
+			}
+			socket_send( $s, $message, strlen( $message ), 0 );
+			socket_close( $s );
+		} catch ( Exception $e ) {
+			echo 'Caught exception: ', $e->getMessage(), "\n";
+		}
+	}
+}
